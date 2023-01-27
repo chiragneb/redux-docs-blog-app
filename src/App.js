30732ce1cca990f6f14a5e546a2fb09320b50a4c
neb03x <neb.3391@gmail.com>
2023-01-27 13:50:57 +0530

    pushing to remote  e🏛️

--- src/App.js
+++ src/App.js
@@ -8,6 +8,14 @@ import {
 
 import { Navbar } from './app/Navbar'
 
+import { PostsList } from './features/posts/PostsList'
+
+import { AddPostForm } from './features/posts/AddPostForm'
+
+import { SinglePostPage } from './features/posts/SinglePostPage'
+
+import { EditPostForm } from './features/posts/EditPostForm'
+
 function App() {
   return (
     <Router>
@@ -18,11 +26,14 @@ function App() {
             exact
             path="/"
             render={() => (
-              <section>
-                <h2>Welcome to the Redux Essentials example app!</h2>
-              </section>
+              <React.Fragment>
+                <AddPostForm />
+                <PostsList />
+              </React.Fragment>
             )}
           />
+          <Route exact path="/posts/:postId" component={SinglePostPage} />
+          <Route exact path="/editPost/:postId" component={EditPostForm} />
           <Redirect to="/" />
         </Switch>
       </div>
--- src/app/Navbar.js
+++ src/app/Navbar.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import { Link } from 'react-router-dom'
+
 export const Navbar = () => {
   return (
     <nav>
@@ -8,6 +10,7 @@ export const Navbar = () => {
 
         <div className="navContent">
           <div className="navLinks"></div>
+           <Link to={"/"}>Posts</Link>
         </div>
       </section>
     </nav>
