05ffb003fcc76eb4c44152a158e45366939ba619
neb03x <neb.3391@gmail.com>
2023-01-29 12:06:42 +0530

    chapter 6 finished redux docs

--- src/App.js
+++ src/App.js
@@ -16,12 +16,19 @@ import { SinglePostPage } from './features/posts/SinglePostPage'
 
 import { EditPostForm } from './features/posts/EditPostForm'
 
+import { UsersList } from './features/users/UsersList'
+
+import { UserPage } from './features/users/UserPage'
+
+import { NotificationsList } from './features/notifications/NotificationsList'
+
 function App() {
   return (
     <Router>
       <Navbar />
       <div className="App">
         <Switch>
+          
           <Route
             exact
             path="/"
@@ -34,6 +41,10 @@ function App() {
           />
           <Route exact path="/posts/:postId" component={SinglePostPage} />
           <Route exact path="/editPost/:postId" component={EditPostForm} />
+          <Route exact path="/users" component={UsersList} />
+          <Route exact path="/users/:usersId" component={UserPage} />
+          <Route exact path="/notifications" component={NotificationsList} />
+       
           <Redirect to="/" />
         </Switch>
       </div>
--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -20,3 +20,10 @@ const usersSlice = createSlice({
 })
 
 export default usersSlice.reducer
+
+export const selectAllUsers = state => state.users 
+
+export const selectUserById = (state, userId) => 
+  state.users.find(user => user.id === userId)
+
+  
